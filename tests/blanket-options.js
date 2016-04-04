9939169d43224e44949e6671657d34ea40fa196a
Jordan Hawker <hawker.jordan@gmail.com>
2016-04-04 00:37:47 -0700

    Upgrade packages via `ember init`
    Add ember-suave
    Convert to 2 space cuz suave...

--- tests/blanket-options.js
+++ tests/blanket-options.js
@@ -2,26 +2,26 @@
 // jscs: disable
 
 var options = {
-    modulePrefix: 'ember-unauthorized',
-    filter: '//.*ember-unauthorized/.*/',
-    antifilter: '//.*(tests|template).*/',
-    loaderExclusions: [],
-    enableCoverage: true,
-    cliOptions: {
-        lcovOptions: {
-            outputFile: 'lcov.dat',
-            renamer: function (moduleName) {
-                var expression = /^ember-unauthorized/;
-                var name = moduleName.replace(/^ember-unauthorized\/config/, 'config');
-                return name.replace(expression, 'app') + '.js';
-            }
-        },
-        reporters: ['lcov'],
-        autostart: true
-    }
+  modulePrefix: 'ember-unauthorized',
+  filter: '//.*ember-unauthorized/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    lcovOptions: {
+      outputFile: 'lcov.dat',
+      renamer: function (moduleName) {
+        var expression = /^ember-unauthorized/;
+        var name = moduleName.replace(/^ember-unauthorized\/config/, 'config');
+        return name.replace(expression, 'app') + '.js';
+      }
+    },
+    reporters: ['lcov'],
+    autostart: true
+  }
 };
 if (typeof exports === 'undefined') {
-    blanket.options(options);
+  blanket.options(options);
 } else {
-    module.exports = options;
+  module.exports = options;
 }
